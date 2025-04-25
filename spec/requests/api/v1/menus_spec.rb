8005e1b0d26f6df5ba25602d1a3b1d8697005349
Guilherme Nunes <guilhermenunes09@protonmail.com>
2025-04-25 10:28:48 -0400

    implement index testing

--- spec/requests/api/v1/menus_spec.rb
+++ spec/requests/api/v1/menus_spec.rb
@@ -2,6 +2,19 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Menus", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    before do
+      create_list(:menu, 3)
+    end
+
+    it "returns a list of menus" do
+      get '/api/v1/menus'
+
+      expect(response).to have_http_status(:ok)
+
+      menus = JSON.parse(response.body)
+
+      expect(JSON.parse(response.body)).to be_an(Array)
+      expect(menus.size).to eq(3)
+    end
   end
 end
