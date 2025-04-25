66673d79b36d624553a64c8630c08377419a3557
Guilherme Nunes <guilhermenunes09@protonmail.com>
2025-04-25 10:55:07 -0400

    feat: Add endpoint to show a single menu item
    - Implement `GET /menus/:id` endpoint to retrieve a specific menu item.
    - Add controller logic to handle fetching a single menu by ID.
    - Include tests to validate:
      - Retrieval of an existing menu item.
      - Error response (404) when the menu item does not exist.

--- app/controllers/api/v1/menus_controller.rb
+++ app/controllers/api/v1/menus_controller.rb
@@ -1,13 +1,27 @@
 class Api::V1::MenusController < ApplicationController
+  before_action :set_menu, only: [:show]
+
   def index
     render json: Menu.all
   end
 
-  def show; end
+  def show
+    render json: @menu
+  end
 
   def create; end
 
   def update; end
 
   def destroy; end
+
+  private
+
+  def set_menu
+    begin
+      @menu = Menu.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: "Menu Not Found" }, status: :not_found
+    end
+  end
 end
--- spec/requests/api/v1/menus_spec.rb
+++ spec/requests/api/v1/menus_spec.rb
@@ -1,20 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Menus", type: :request do
-  describe "GET /index" do
-    before do
-      create_list(:menu, 3)
-    end
+  let!(:menus) { create_list(:menu, 3) }
 
+  describe "GET /index" do
     it "returns a list of menus" do
       get '/api/v1/menus'
 
       expect(response).to have_http_status(:ok)
+      parsed_menus = JSON.parse(response.body)
 
-      menus = JSON.parse(response.body)
+      expect(parsed_menus).to be_an(Array)
+      expect(parsed_menus.size).to eq(3)
+    end
+  end
 
-      expect(JSON.parse(response.body)).to be_an(Array)
-      expect(menus.size).to eq(3)
+  describe "GET /show" do
+    context "menu exists" do
+      it "returns the selected menu" do
+        first_menu_id = menus.first.id
+
+        get "/api/v1/menus/#{first_menu_id}"
+
+        expect(response).to have_http_status(:ok)
+        menu = JSON.parse(response.body)
+
+        expect(menu["id"]).to eq(first_menu_id)
+      end
+    end
+
+    context "menu doesn't exist" do
+      it "returns a not found message" do
+        first_menu_id = menus.first.id
+        get "/api/v1/menus/#{first_menu_id + 10}"
+
+        parsed_response = JSON.parse(response.body)
+        expect(response).to have_http_status(:not_found)
+        expect(parsed_response).to include("error" => "Menu Not Found")
+      end
     end
   end
 end
