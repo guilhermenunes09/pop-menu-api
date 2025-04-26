c23e5e8a04754cf267da18944580e8a250b7b659
Guilherme Nunes <guilhermenunes09@protonmail.com>
2025-04-26 19:04:28 -0400

    rubocop autocorrect

--- spec/models/menu_item_spec.rb
+++ spec/models/menu_item_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe MenuItem, type: :model do
 
   describe "validations" do
     it "is valid with valid attributes" do
-      menu_item = create(:menu_item, restaurant: )
+      menu_item = create(:menu_item, restaurant:)
       expect(menu_item).to be_valid
     end
 
     it "is invalid without a name" do
-      menu_item = create(:menu_item, restaurant: )
+      menu_item = create(:menu_item, restaurant:)
       menu_item.name = nil
       expect(menu_item).not_to be_valid
     end
