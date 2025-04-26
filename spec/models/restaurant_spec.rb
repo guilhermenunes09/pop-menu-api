22fc2e2dee3ed2517d8096fdb34c4052458a0ae2
Guilherme Nunes <guilhermenunes09@protonmail.com>
2025-04-26 19:03:57 -0400

    add models spec

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it "has many menus" do
+      restaurant = create(:restaurant)
+      menu1 = create(:menu, restaurant: restaurant)
+      menu2 = create(:menu, restaurant: restaurant)
+
+      expect(restaurant.menus).to include(menu1, menu2)
+    end
+
+    it "has many menu items" do
+      restaurant = create(:restaurant)
+      menu_item1 = create(:menu_item, restaurant: restaurant)
+      menu_item2 = create(:menu_item, restaurant: restaurant)
+
+      expect(restaurant.menu_items).to include(menu_item1, menu_item2)
+    end
+  end
+
+  describe "dependent destroy" do
+    it "destroys associated menus when the restaurant is destroyed" do
+      restaurant = create(:restaurant)
+      menu = create(:menu, restaurant: restaurant)
+
+      expect { restaurant.destroy }.to change(Menu, :count).by(-1)
+    end
+
+    it "destroys associated menu items when the restaurant is destroyed" do
+      restaurant = create(:restaurant)
+      menu_item = create(:menu_item, restaurant: restaurant)
+
+      expect { restaurant.destroy }.to change(MenuItem, :count).by(-1)
+    end
+  end
+
+  describe "validations" do
+    it "is valid with a name" do
+      restaurant = build(:restaurant, name: "Tasty Bites")
+      expect(restaurant).to be_valid
+    end
+
+    it "is invalid without a name" do
+      restaurant = build(:restaurant, name: nil)
+      expect(restaurant).not_to be_valid
+      expect(restaurant.errors[:name]).to include("can't be blank")
+    end
+  end
 end
